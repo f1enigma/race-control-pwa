307e16e624d55fae3a5a8c75d9578097866e805e
f1enigma <145000854+f1enigma@users.noreply.github.com>
2026-07-29 18:40:38 +0300

    Fix TDZ error on REDDIT_SUBS, bump sw cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "race-control-v2";
+const CACHE = "race-control-v3";
 const SHELL = ["./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", e => {
